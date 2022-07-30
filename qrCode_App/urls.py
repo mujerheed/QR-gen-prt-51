fd6f1b4ad2d054ed2e14197a34543d663b7816d3
Abdurrazaq Jibril Baba <mujerheed20@gmail.com>
2022-07-30 10:45:26 +0100

    Account: Update login.html

--- qrCode_App/urls.py
+++ qrCode_App/urls.py
@@ -1,7 +1,6 @@
 
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, LoginPageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='Index'),
-    path('l')
 ]
